78158984a99d5b902f98a1a54bf3cffaa8ca20d9
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-03-06 16:20:07 -0300

    passando a utilizar HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -17,7 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClient
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
