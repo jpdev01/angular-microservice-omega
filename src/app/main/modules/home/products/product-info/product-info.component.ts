8187012529223d226bf0dcdda8c203878a439afe
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-03-21 00:55:08 -0300

    resolvendo problema de rotas + navbar

--- src/app/main/modules/home/products/product-info/product-info.component.ts
+++ src/app/main/modules/home/products/product-info/product-info.component.ts
@@ -1,5 +1,7 @@
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../../../shared/service/products.service';
+import { InterfaceRule } from '../../../../shared/model/interface-rule.model';
 
 @Component({
   selector: 'app-product-info',
@@ -9,14 +11,20 @@ import { Component, OnInit } from '@angular/core';
 export class ProductInfoComponent implements OnInit {
 
   productId;
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute, private service: ProductsService) { }
 
   ngOnInit() {
+    this.showNavbar(false);
     this.getParams();
   }
 
   getParams(): void {
     this.route.params.subscribe(params => this.productId = params['id']);
+  }
+
+  private showNavbar(show: boolean) {
+    let interfaceRuleNavbar = new InterfaceRule("navbar", show, "");
+    this.service.interfaceRuleEmitter.emit(interfaceRuleNavbar);
   }
 
   // getParams(): void {
--- src/app/main/modules/home/products/product-list/product-list.component.ts
+++ src/app/main/modules/home/products/product-list/product-list.component.ts
@@ -46,7 +46,7 @@ export class ProductListComponent implements OnInit {
 
   private redirectToProductInfo(id: number){
     // this.router.navigate(['home/product/info'], { queryParams: { id: id } });
-    this.router.navigate(['home/user/info', id]);
+    this.router.navigate(['home/product/info', id]);
   }
 
   private showNavbar(): void {
