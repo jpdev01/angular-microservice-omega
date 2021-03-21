e0ec79c2c05555c2ad470d7edb1193d7e5c051f9
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-03-21 11:28:45 -0300

    refazendo os metodos de api

--- src/app/main/shared/service/user-api.service.ts
+++ src/app/main/shared/service/user-api.service.ts
@@ -1,24 +1,36 @@
-import { HttpClient } from '@angular/common/http';
+import { User } from './../model/user.model';
+import { Utils } from './../utils/Utils.model';
+import { ResponsePageable } from './../model/responsePageable.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserApiService {
 
-  constructor(private httpClient: HttpClient) { }
+  apiUrl: string;
+  httpOptions: object;
 
-  public getUsers(): Observable<ResponsePageable> {
+  constructor(private httpClient: HttpClient, private utils: Utils) { }
+
+  onInit(): void {
+    this.apiUrl = this.utils.getApiUrl();
+    this.httpOptions = this.utils.getHttpOptions();
+  }
+
+  public getAll(): Observable<ResponsePageable> {
     debugger;
     return this.httpClient.get<ResponsePageable>(this.apiUrl + '/user');
   }
 
-  public getUser(id: number): Observable<ResponsePageable> {
+  public getById(id: number): Observable<ResponsePageable> {
     debugger;
     return this.httpClient.get<ResponsePageable>(this.apiUrl + '/user/' + id);
   }
 
-  public saveUser(user: any): Observable<User> {
+  public save(user: any): Observable<User> {
     return this.httpClient.post<any>(this.apiUrl + '/user/save', user, this.httpOptions);
   }
 
--- src/app/main/shared/utils/Utils.model.ts
+++ src/app/main/shared/utils/Utils.model.ts
@@ -6,22 +6,26 @@ import { Subject } from 'rxjs';
 // @Injectable()
 export class Utils {
 
-  apiUrl: string = "http://localhost:8080/neusamoda";
+  private apiUrl: string = "http://localhost:8080/neusamoda";
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type' : 'application/json'
+    })
+  };
 
   constructor() { }
 
-  getApiUrl(): string {
+  public getApiUrl(): string {
     return this.apiUrl;
+  }
+
+  public getHttpOptions(): object {
+    return this.httpOptions;
   }
 
   // filter!
   emitterFilterChange = new EventEmitter<string>();
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type' : 'application/json'
-    })
-  };
 
 
 
