7cfde721ca8b905f8076fd770967988c1610fed0
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-03-21 00:28:00 -0300

    corrigindo problema de navbar que nao escondia -> products

--- src/app/main/modules/home/products/products.component.ts
+++ src/app/main/modules/home/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InterfaceRule } from 'src/app/main/shared/model/interface-rule.model';
+import { ProductsService } from 'src/app/main/shared/service/products.service';
 
 @Component({
   selector: 'app-products',
@@ -9,9 +11,25 @@ export class ProductsComponent implements OnInit {
 
   navbarShow = false;
 
-  constructor() { }
+  constructor(private service: ProductsService) { }
 
   ngOnInit(): void {
+    this.interfaceRuleNavbar();
+  }
+
+  interfaceRuleNavbar():void {
+    this.service.interfaceRuleEmitter.subscribe(
+      (interfaceRule: InterfaceRule) => {
+        if (interfaceRule.element === 'navbar') {
+          if (interfaceRule.show == true) {
+            this.navbarShow = true;
+
+          } else {
+            this.navbarShow = false;
+          }
+        }
+      }
+    );
   }
 
 }
