3bff6b9992f4d6340b90f47d7bbd004865cd94b6
João Pedro Truchinski Borba <joao.borba@neomind.com.br>
2021-04-02 13:14:06 -0300

    backup

--- src/app/main/shared/model/form-serializer.model.ts
+++ src/app/main/shared/model/form-serializer.model.ts
@@ -7,7 +7,6 @@ export class FormSerializer {
   id: string;
   name: string;
   groups: FormGroupSerializer[];
-  //groups: [{key: string; field: FormField[]}];
   formGroupName: string;
   configs: object;
   serviceApi: any;
@@ -24,44 +23,10 @@ export class FormSerializer {
 
     this.id = options.id;
     this.name = options.name;
-
-    // this.initGroupsFields(options);
     this.groups = options.groups ? options.groups : [];
     this.formGroupName = options.formGroupName;
     this.configs = options.configs;
     this.entityName = options.entityName;
     this.serviceApi = options.serviceApi;
-
   }
-
-  // private initGroupsFields(options: any): void{
-  //   let add = false;
-  //   options.fields.forEach((field) => {
-  //     let row = field.row;
-  //     if (this.groups) {
-  //       this.groups.forEach((group) => {
-  //         if (group.key === row) {
-  //           group.field.push(field);
-  //           add = true;
-  //         }
-  //       });
-  //       if ( add === false ) {
-  //         this.groups.push({
-  //           key: '1',
-  //           field: field
-  //         });
-  //       }
-  //     } else {
-  //       this.groups = [{
-  //         key: field.row,
-  //         field: field
-  //       }];
-  //     }
-
-
-  //   });
-
-  // }
-
-
 }
