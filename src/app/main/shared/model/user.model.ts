01087b6593d972a873275c2b220724d84a596c61
João Pedro Truchinski Borba <joao.borba@neomind.com.br>
2021-04-02 12:36:40 -0300

    add constructor na classe de user

--- src/app/main/shared/model/user.model.ts
+++ src/app/main/shared/model/user.model.ts
@@ -11,6 +11,21 @@ export class User
   active = Active.ACTIVE;
   customer: Customer;
 
+  constructor(options: {
+    id?: number;
+    login?: string;
+    password?: string;
+    permission?: Permission;
+    active?: Active;
+    customer?: Customer;
+  } = {}){
+    this.id = options.id;
+    this.login = options.login;
+    this.password = options.password;
+    this.permission = options.permission;
+    this.active = options.active;
+    this.customer = options.customer;
+  }
   public getPermission(): Permission {
     return this.permission;
   }
