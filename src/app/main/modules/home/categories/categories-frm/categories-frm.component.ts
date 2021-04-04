76b74f865b9404e274d91f584d48eb229878b060
João Pedro Truchinski Borba <joao.borba@neomind.com.br>
2021-04-03 22:17:21 -0300

    criacao do componente de formulario de categoria

---
+++ src/app/main/modules/home/categories/categories-frm/categories-frm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-frm',
+  templateUrl: './categories-frm.component.html',
+  styleUrls: ['./categories-frm.component.css']
+})
+export class CategoriesFrmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/main/modules/home/categories/categories.module.ts
+++ src/app/main/modules/home/categories/categories.module.ts
@@ -5,10 +5,11 @@ import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesComponent } from './categories.component';
 import { CategoriesListComponent } from './categories-list/categories-list.component';
 import { ListEntityModule } from 'src/app/main/components/list-entity/list-entity.module';
+import { CategoriesFrmComponent } from './categories-frm/categories-frm.component';
 
 
 @NgModule({
-  declarations: [CategoriesComponent, CategoriesListComponent],
+  declarations: [CategoriesComponent, CategoriesListComponent, CategoriesFrmComponent],
   imports: [
     CommonModule,
     CategoriesRoutingModule,
