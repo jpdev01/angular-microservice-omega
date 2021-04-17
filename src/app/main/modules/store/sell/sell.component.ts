59f6646496b029c977421cf29a59048cf49d01ba
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-04-16 21:04:00 -0300

    recebimento de produto via code scan

--- src/app/main/modules/store/sell/sell.component.ts
+++ src/app/main/modules/store/sell/sell.component.ts
@@ -1,3 +1,5 @@
+import { ProductsApiService } from './../../../shared/service/products-api.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EntityListSerialize } from './../../../shared/serialize/entity-list-serialize.model';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/main/shared/model/product.model';
@@ -9,11 +11,42 @@ import { Product } from 'src/app/main/shared/model/product.model';
 })
 export class SellComponent implements OnInit {
   listData;
+  typeInput = TypeInput.SCANNER;
+  frm: FormGroup;
+  codeScan;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private productApi: ProductsApiService
+  ) { }
 
   ngOnInit() {
     this.initListData();
+    this.initFrm();
+  }
+
+  private initFrm(){
+    this.frm = this.fb.group({
+      codeScan: [this.codeScan, [Validators.required]],
+      typeInput: [this.typeInput, []]
+    });
+  }
+
+  keyCode($event): void {
+    let code = $event.target.value;
+    if (code){
+      if (this.typeInput == TypeInput.SCANNER){
+        this.getProductById(code);
+        //this.frm.controls['codeScan'].setValue("");
+        this.codeScan = "";
+      }
+    }
+  }
+
+  private getProductById(code: any){
+    this.productApi.getById(code).subscribe((product)=>{
+      this.listData.entity.push(product);
+    });
   }
 
   public initListData(): void {
@@ -23,17 +56,22 @@ export class SellComponent implements OnInit {
     };
 
     this.listData = new EntityListSerialize({
-      entity: {},
+      entity: [],
       tableStructure: tableInfo
     });
+  }
 
-      console.log("Hello");
-      this.listData.entity = [new Product({
-        name: "Teste",
-        code: "1",
-        finalValue: 120
-      })]
+  public addFieldToArray(product: Product) {
+    this.listData.entity.push(product);
+  }
+
+  changeMethodInput($event){
+      this.typeInput = $event.target.value;
   }
 
 
+}
+
+enum TypeInput {
+  KEYBOARD, SCANNER
 }
