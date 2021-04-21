54b861c77bfe2dd2431be2684f87bc2a9e4c3c23
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-04-21 20:04:15 -0300

    edicao de categoria em modal

--- src/app/main/components/modal/modal.module.ts
+++ src/app/main/components/modal/modal.module.ts
@@ -1,4 +1,3 @@
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal.component';
--- src/app/main/modules/home/categories/categories-frm/categories-frm.component.ts
+++ src/app/main/modules/home/categories/categories-frm/categories-frm.component.ts
@@ -114,6 +114,10 @@ export class CategoriesFrmComponent implements OnInit, EntityFormInterfaceCompon
     }
   }
 
+  ngOnChanges(){
+    this.ngOnInit();
+  }
+
 
 
 }
