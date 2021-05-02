3da1464666240dddbc7cb6f6ad1bac3c2b9b9e7f
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-05-02 09:56:39 -0300

    ajustes na criacao de formulario totalmente dinamico

--- src/app/main/components/eform/eform/eform.module.ts
+++ src/app/main/components/eform/eform/eform.module.ts
@@ -2,12 +2,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EformComponent } from './eform.component';
+import { HeaderModule } from '../header/header.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HeaderModule
   ],
   declarations: [EformComponent], exports: [EformComponent]
 })
