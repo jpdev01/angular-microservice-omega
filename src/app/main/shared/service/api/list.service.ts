f2c10910b0b8a52d6441e690aae8c49066424fa7
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-05-05 20:52:13 -0300

    list api service

---
+++ src/app/main/shared/service/api/list.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Utils } from '../../utils/Utils.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListService {
+  apiUrl: string;
+  httpOptions: object;
+
+  constructor(private httpClient: HttpClient, private utils: Utils) {
+    this.apiUrl = this.utils.getApiUrl();
+    this.httpOptions = this.utils.getHttpOptions();
+  }
+
+  public get(component: string): Observable<any> {
+    return this.httpClient.get<any>(this.apiUrl  + "/" + component, this.httpOptions);
+  }
+
+}
