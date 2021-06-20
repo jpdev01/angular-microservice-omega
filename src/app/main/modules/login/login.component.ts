11a7a463fe7d142784c6e5efa2f025025b327747
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-06-19 22:11:48 -0300

    colocado timeOut de login para garantir que o token esteja definido

--- src/app/main/modules/login/login.component.ts
+++ src/app/main/modules/login/login.component.ts
@@ -45,7 +45,7 @@ export class LoginComponent implements OnInit {
         localStorage.setItem('access_token', resultSuccess.token);
         this.authService.login();
         this.userIsLogged = true;
-        this.redirectToHome();
+        window.setTimeout(()=>{this.redirectToHome()}, 300)
     },
     (resultError) => {
       if (resultError.status == '403') {
